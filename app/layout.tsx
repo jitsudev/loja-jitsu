2deb0ec52ae9aa601a8f10a052c6d1c6f90cf34e
Felipe Marinho <felipegigamail@gmail.com>
2024-06-10 15:50:28 -0300

    Added speed insights scripts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -16,6 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <SpeedInsights />
       <body className={inter.className}>{children}</body>
     </html>
   );
