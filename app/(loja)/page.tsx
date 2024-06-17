d25989b5d43eeadc14a3d2e8da1f570a2a730f54
Felipe Marinho <felipegigamail@gmail.com>
2024-06-17 15:47:15 -0300

    Fix produto

--- app/(loja)/page.tsx
+++ app/(loja)/page.tsx
@@ -5,7 +5,7 @@ export default function Page() {
     <>
       <h1>Página inicial com destaques e lançamentos...</h1>
       {[...Array(10)].map((e, i) => (
-        <Produto key={i} slug="camisa-estampada" />
+        <Produto key={i} tipo="camisa" slug="camisa-estampada" />
       ))}
     </>
   );
