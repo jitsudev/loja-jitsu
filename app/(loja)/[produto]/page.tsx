20a1c7d0baf2266c5a112ecd4f59fe4c71c64a99
Felipe Marinho <felipegigamail@gmail.com>
2024-06-23 10:35:08 -0300

    feat: connection to database

--- app/(loja)/[produto]/page.tsx
+++ app/(loja)/[produto]/page.tsx
@@ -6,13 +6,15 @@ import { PrismaClient, Produto } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export default async function Page({ params }: { params: { produto: string } }) {
+	const user = await prisma.user.findMany();
+	console.log(user);
 	const produtos = await prisma.produto.findMany();
 	console.log(produtos);
 	return (
 		<>
 			<Filtros />
 			{produtos?.map((e, i) => (
-				<CardProduto key={i} title={e.title} tipo={params.produto} slug={e.slug} price={e.price} />
+				<CardProduto key={i} title={e.title} tipo={params.produto} slug={e.slug} price={e.price.toString()} />
 			))}
 		</>
 	);
