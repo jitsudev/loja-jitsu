a3e5d426d8c928ed8f89e8474c8aeb37f7db6696
Felipe Marinho <felipegigamail@gmail.com>
2024-06-23 08:44:57 -0300

    fix: remove missing imports

--- app/auth/recuperasenha/page.tsx
+++ app/auth/recuperasenha/page.tsx
@@ -1,42 +1,25 @@
-import Produto from "@/app/components/produto";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function Page() {
-  return (
-    <div className="flex flex-col w-full min-h-svh justify-center items-center">
-      <div className="flex flex-col w-1/2 justify-center items-center gap-4 mb-20">
-        <Image
-          src="/logo_footer.png"
-          width={180}
-          height={80}
-          alt="Logo Jitsu"
-        />
-        <h1>Esqueceu sua senha ? Digite seu e-mail.</h1>
-        <form className="flex flex-col gap-4">
-          <input
-            type="text"
-            name="email"
-            placeholder="E-Mail"
-            className="bg-transparent border-solid border-2 p-2 rounded-lg"
-          />
-          <button
-            type="submit"
-            className="flex justify-center items-center w-full p-2 bg-green-600 rounded-full"
-          >
-            Reenviar a senha
-          </button>
-        </form>
-      </div>
-      <div className="flex flex-col justify-center items-center gap-4">
-        <h2>Lembrou a senha ?</h2>
-        <Link
-          href="/auth/login"
-          className="flex justify-center items-center w-full p-2 bg-blue-800 rounded-full"
-        >
-          Login
-        </Link>
-      </div>
-    </div>
-  );
+	return (
+		<div className="flex flex-col w-full min-h-svh justify-center items-center">
+			<div className="flex flex-col w-1/2 justify-center items-center gap-4 mb-20">
+				<Image src="/logo_footer.png" width={180} height={80} alt="Logo Jitsu" />
+				<h1>Esqueceu sua senha ? Digite seu e-mail.</h1>
+				<form className="flex flex-col gap-4">
+					<input type="text" name="email" placeholder="E-Mail" className="bg-transparent border-solid border-2 p-2 rounded-lg" />
+					<button type="submit" className="flex justify-center items-center w-full p-2 bg-green-600 rounded-full">
+						Reenviar a senha
+					</button>
+				</form>
+			</div>
+			<div className="flex flex-col justify-center items-center gap-4">
+				<h2>Lembrou a senha ?</h2>
+				<Link href="/auth/login" className="flex justify-center items-center w-full p-2 bg-blue-800 rounded-full">
+					Login
+				</Link>
+			</div>
+		</div>
+	);
 }
